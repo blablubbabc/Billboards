7b91dbabc55b9d41a4c46c37d9474a9d44a0e847
blablubbabc <lukas@wirsindwir.de>
2018-05-29 13:43:52 +0200

    Converted to maven project.
    Updated dependencies to Vault 1.6 and Bukkit 1.8
    Added LICENSE file.
    
    Fixed compatibility with Bukkit 1.12+

---
+++ Billboards/src/main/java/de/blablubbabc/billboards/BillboardSign.java
@@ -0,0 +1,89 @@
+package de.blablubbabc.billboards;
+
+public class BillboardSign {
+
+	private SoftLocation location;
+	private String creatorName;
+	private String ownerName;
+	private int durationInDays;
+	private int price;
+	private long startTime;
+
+	public BillboardSign(SoftLocation location, String creatorName, String ownerName, int durationInDays, int price, long startTime) {
+		this.location = location;
+		this.creatorName = (creatorName == null || creatorName.isEmpty()) ? "SERVER" : creatorName;
+		this.ownerName = (ownerName == null || ownerName.isEmpty()) ? "SERVER" : ownerName;
+		this.durationInDays = durationInDays;
+		this.price = price;
+		this.startTime = startTime;
+	}
+
+	public SoftLocation getLocation() {
+		return location;
+	}
+
+	public void setLocation(SoftLocation location) {
+		this.location = location;
+	}
+
+	public String getCreatorName() {
+		return creatorName;
+	}
+
+	public boolean hasCreator() {
+		return !creatorName.equals("SERVER");
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public boolean hasOwner() {
+		return !ownerName.equals("SERVER");
+	}
+
+	public void setOwner(String owner) {
+		this.ownerName = owner == null ? "SERVER" : owner;
+	}
+
+	public void resetOwner() {
+		this.setOwner(null);
+		startTime = 0;
+	}
+
+	public int getDurationInDays() {
+		return durationInDays;
+	}
+
+	public void setDurationInDays(int durationInDays) {
+		this.durationInDays = durationInDays;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public long getEndTime() {
+		return startTime + (durationInDays * 24 * 3600 * 1000);
+	}
+
+	public long getTimeLeft() {
+		return this.getEndTime() - System.currentTimeMillis();
+	}
+
+	public boolean isRentOver() {
+		return !hasOwner() || this.getTimeLeft() <= 0;
+	}
+}
---
+++ Billboards/src/main/java/de/blablubbabc/billboards/SignEdit.java
@@ -0,0 +1,14 @@
+package de.blablubbabc.billboards;
+
+import org.bukkit.Location;
+
+public class SignEdit {
+
+	public final BillboardSign billboard;
+	public final Location source;
+
+	public SignEdit(Location source, BillboardSign billboard) {
+		this.source = source;
+		this.billboard = billboard;
+	}
+}
